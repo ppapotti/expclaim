e816fb696641202de12c3c21ad455e31efdff854
MhmdSaiid <35137980+MhmdSaiid@users.noreply.github.com>
2019-12-16 23:17:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#./gdown.pl https://drive.google.com/open?id=1XqH_efF_TbsC15Klm0J_GgELOedJc9E9 scripts/Bing/FactCheck.jar
+./gdown.pl https://drive.google.com/open?id=1XqH_efF_TbsC15Klm0J_GgELOedJc9E9 scripts/Bing/FactCheck.jar
 
 conda install -y -c potassco clingo
 conda install -y -c anaconda ply
